ba4224239dd66240e0ce388eb924e9393d106557
rjegge <39504113+rjegge@users.noreply.github.com>
2020-06-02 21:46:13 -0400

    First
    A first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import "./styles.css";
+import React from 'react';
+import './styles.css';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello World</h1>
+      <h2>A playground app from Rob...</h2>
     </div>
   );
 }
